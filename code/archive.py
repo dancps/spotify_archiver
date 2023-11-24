7549e022f9d617add6ffa7f4d7bab1d8ebd3fdbe
Danilo Calhes <dancps96@gmail.com>
2023-11-24 19:56:10 -0300

    Fixing some format errors

--- code/archive.py
+++ code/archive.py
@@ -2,26 +2,23 @@
 
 '''
 import requests
-import json
 from math import ceil
 from termcolor import colored
-import colorama # this is necessary due to ANSI color incompatibility
-import pandas as pd
+import colorama  # this is necessary due to ANSI color incompatibility
 import os
-import numpy as np
 from tqdm import tqdm
 import datetime
 import wget
 import argparse
-from time import sleep #dbug only
 import emoji
 import csv
 
-def get_playlist_tracks(playlist_id,token,limit=100,offset=0):
+
+def get_playlist_tracks(playlist_id, token, limit=100, offset=0):
     '''
     Gets a Spotify request for the tracks of the playlists.
 
-    Input: 
+    Input:
         playlist_id - id of the chosen playlist
         token - authentication token
         limit -  number of playlists in the request
@@ -29,11 +26,13 @@ def get_playlist_tracks(playlist_id,token,limit=100,offset=0):
     Output:
         r - Spotify request
     '''
-    if(limit==None or limit>100 or limit<0):
+    if (limit is None or limit > 100 or limit < 0):
         raise ValueError("Choose a valid value for limit parameter. (0>limit>=100)")
-    if(offset==None or offset<0):
+    if (offset is None or offset < 0):
         raise ValueError("Choose a valid value for offset parameter. (offset>0)")
-    url="https://api.spotify.com/v1/playlists/{}/tracks?limit={}&offset={}".format(playlist_id,limit,offset)
+    url = "https://api.spotify.com/v1/playlists/{}/tracks?limit={}&offset={}".format(
+        playlist_id,limit,offset
+        )
 
     headers = {
         "Accept": "application/json",
@@ -45,29 +44,31 @@ def get_playlist_tracks(playlist_id,token,limit=100,offset=0):
 
     return r
 
+
 def get_tracks_list(playlist_id,token):
     '''
-    
 
-    Input: 
+
+    Input:
         playlist_id - id of the chosen playlist
         token - authentication token
     Output:
         tracks_list - list of tracks
     '''
     tracks_list = []
-    # Calculates the size of 
+    # Calculates the size of
     range_steps =range(calculate_request_steps(get_playlist_tracks(playlist_id,token),n=100))
     for step in range_steps:
         r = get_playlist_tracks(playlist_id,token,limit=100,offset=step*100)
         tracks_list.append(r)
     return tracks_list
 
+
 def get_playlist_cover(playlist_id,token): # Ref: https://developer.spotify.com/console/get-playlist-images/
     '''
     Gets the cover image link of the playlist.
 
-    Input: 
+    Input:
         playlist_id - id of the chosen playlist
         token - authentication token
     Output:
@@ -83,11 +84,12 @@ def get_playlist_cover(playlist_id,token): # Ref: https://developer.spotify.com/
     r = requests.get(url, headers=headers)
     return r
 
+
 def get_playists(user,token,limit=50,offset=0):
     '''
     Gets a Spotify request for the playlists.
 
-    Input: 
+    Input:
         user - string containing the user
         token - authentication token
         limit -  number of playlists in the request
@@ -109,31 +111,33 @@ def get_playists(user,token,limit=50,offset=0):
     }
 
     r = requests.get(playlists_url, headers=headers)
-    
+
     return r
+
 
 def get_playists_list(user,token):
     '''
     Uses calculate_requset_step to get a list of available playlists. I.e., if there are 180 playlists, this will return a 4 items list with a maximum of 50 playlists in each of them.
 
-    Input: 
+    Input:
         user - string containing the user
         token - authentication token
     Output:
         playlists_list - list of playlists
     '''
     playlists_list = []
-    # Calculates the size of 
+    # Calculates the size of
     for step in range(calculate_request_steps(get_playists(user,token))):
         r = get_playists(user,token,limit=50,offset=step*50)
         playlists_list.append(r)
     return playlists_list
 
-def calculate_request_steps(request,n=50,total=False):
+
+def calculate_request_steps(request, n=50,total=False):
     '''
     Calculates the number of required steps to get all of the available requests.
 
-    Input: 
+    Input:
         request - Spotify request
     Output:
         steps - number of steps to cover all the playlists
@@ -145,12 +149,13 @@ def calculate_request_steps(request,n=50,total=False):
     if(total): return steps, total_n
     else: return steps
 
+
 def main():
     parser = argparse.ArgumentParser(description='Saves the playlists of the user into jsons and csvs.')
-    parser.add_argument('-u','--user', required=True, help='set the user')
-    parser.add_argument('-t','--token_file', default=".auth/tokens.txt", help='specifies the text file containing user token')
+    parser.add_argument('-u', '--user', required=True, help='set the user')
+    parser.add_argument('-t', '--token_file', default=".auth/tokens.txt", help='specifies the text file containing user token')
     parser.add_argument('--user_only', action='store_true', help='store only playlists owned by specified user')
-    parser.add_argument('-f','--force', action='store_true', help='forces to save(this will overwrite the file if it already exists)')
+    parser.add_argument('-f', '--force', action='store_true', help='forces to save(this will overwrite the file if it already exists)')
     args = parser.parse_args()
 
     token_path = os.path.join(args.token_file)
@@ -170,13 +175,13 @@ def main():
         if(not(os.path.isdir(json_path))):
             os.makedirs(json_path)
 
-        
+
         for step,plst_step in enumerate(plst_lst):
             with open(os.path.join('json','playlists_part_{}.json'.format(step)),'w') as fl:
                 fl.write(plst_step.text)
 
         id_list = [(playlists['id'],playlists['name'],playlists['owner']['id']) for pl_step in plst_lst for playlists in pl_step.json()['items']]
-        
+
         print("Getting "+colored(args.user,'blue',attrs=['bold'])+" and "+colored('others','red',attrs=['bold'])+" playlists.")
         progbar = tqdm(id_list,bar_format='{desc} {percentage:3.0f}%|{bar}{r_bar}',dynamic_ncols=True)
         for id,name,owner in progbar:
@@ -185,10 +190,10 @@ def main():
             for l in show_name:
                 if l in emoji.UNICODE_EMOJI:
                     show_name = show_name.replace(l,"")
-        
-            if(owner==args.user): 
+
+            if(owner==args.user):
                 progbar.set_description(colored("{}".format(show_name[:20]),'blue',attrs=['bold']))
-                
+
                 # Creates the json playlist folder
                 playlist_json_path = os.path.join('json','playlists',args.user,name.replace("/","_"))
                 if(not(args.force) and os.path.isdir(playlist_json_path)): continue # deletes after
@@ -199,10 +204,10 @@ def main():
                 playlist = get_tracks_list(id,token)
                 for playlist_part_indx,playlist_part in enumerate(playlist):
                     playlist_json = os.path.join(playlist_json_path,'{}_{}.json'.format(name.replace("/","_"),playlist_part_indx))
-                    if(not(args.force) and os.path.isfile(playlist_json)): continue 
+                    if(not(args.force) and os.path.isfile(playlist_json)): continue
                     with open(playlist_json,'w') as fl:
                         fl.write(playlist_part.text)
-                
+
                 # Gets the info for each music
                 music = [mus['track']['name'] for play in playlist for mus in play.json()['items']]
                 album = [mus['track']['album']['name'] for play in playlist for mus in play.json()['items']]
@@ -210,8 +215,8 @@ def main():
                 duration_ms = [mus['track']['duration_ms'] for play in playlist for mus in play.json()['items']]
                 added_at = [mus['added_at'] for play in playlist for mus in play.json()['items']]
                 added_by = [mus['added_by']['id'] for play in playlist for mus in play.json()['items']]
-                
-                
+
+
                 # Creates a folder to store the csv and cover image
                 playlist_path = os.path.join('playlists',args.user,name.replace("/","_"))
                 if(not(os.path.isdir(playlist_path))):
@@ -220,7 +225,7 @@ def main():
                 # Saves the cover image
                 cover_url = get_playlist_cover(id,token).json()[0]['url']
                 wget.download(cover_url, out=os.path.join(playlist_path,"{}_cover.png".format(name.replace("/","_"))), bar="")
-                
+
                 # Saves the playlist in a csv
                 playlist_csv_path = os.path.join(playlist_path,name.replace("/","_")+'.csv')
                 with open(playlist_csv_path, 'w', newline='') as csvfile:
@@ -230,9 +235,9 @@ def main():
                         writer.writerow([music[mu],album[mu],artist[mu],duration_ms[mu],added_at[mu],added_by[mu]])
 
 
-            else: 
+            else:
                 progbar.set_description(colored("{}".format(show_name[:20]),'red',attrs=['bold']))
-                
+
                 # Creates the json playlist folder
                 if(not(args.user_only)):
                     playlist_json_path = os.path.join('json','playlists',"other",name.replace("/","_"))
@@ -243,7 +248,7 @@ def main():
                     playlist = get_tracks_list(id,token)
                     for playlist_part_indx,playlist_part in enumerate(playlist):
                         playlist_json = os.path.join(playlist_json_path,'{}_{}.json'.format(name.replace("/","_"),playlist_part_indx))
-                        if(not(args.force) and os.path.isfile(playlist_json)): continue 
+                        if(not(args.force) and os.path.isfile(playlist_json)): continue
                         with open(playlist_json,'w') as fl:
                             fl.write(playlist_part.text)
 
@@ -254,7 +259,7 @@ def main():
                     duration_ms = [mus['track']['duration_ms'] for play in playlist for mus in play.json()['items']]
                     added_at = [mus['added_at'] for play in playlist for mus in play.json()['items']]
                     added_by = [mus['added_by']['id'] for play in playlist for mus in play.json()['items']]
-                    
+
                     # Creates a folder to store the csv and cover image
                     playlist_path = os.path.join('playlists',"other",name.replace("/","_"))
                     if(not(os.path.isdir(playlist_path))):
@@ -271,7 +276,7 @@ def main():
                         writer.writerow(['name','album','artist','duration_ms','added_at','added_by'])
                         for mu in range(len(music)):
                             writer.writerow([music[mu],album[mu],artist[mu],duration_ms[mu],added_at[mu],added_by[mu]])
-                
+
 
 
 if(__name__=="__main__"):
